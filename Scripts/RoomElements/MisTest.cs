1dcd3378846d262466fa017b8112381e687b5b93
1i9ht <846017531@qq.com>
2016-12-25 20:39:33 +0800

    LKS 12.25-2

---
+++ Scripts/RoomElements/MisTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MisTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        Missile missile = new Missile();
+        missile.SetFlyDistance(5.0f);
+        missile.SetFlySpeed(1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
