9249a559c818f6a82641e42560f66ff1781a377b
cen20081007 <cen20081007@163.com>
2017-03-03 14:08:14 +0800

    Cen-3.3-0

---
+++ Scripts/Buff/BuffAttack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuffAttack : Buff {
+    /// <summary>
+    /// buff触发的概率
+    /// </summary>
+    int probability;
+    public int Probability
+    {
+        get { return probability; }
+        set { probability = value; }
+    }
+
+    bool JudgeTrigger(){
+        //创建random的实例
+        System.Random random = new System.Random();
+        if (random.Next(100) <= probability)
+            return true;
+        else
+            return false;
+    }
+
+
+}
