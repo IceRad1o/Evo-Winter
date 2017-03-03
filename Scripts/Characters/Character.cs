075c5cc0fcbbba35b9e5d8b3128e7019762ef434
GoodBye-CurelWorld <421790777@qq.com>
2017-03-03 16:27:57 +0800

    yyf-17.3.3.8

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -342,7 +342,7 @@ public class Character : Subject
 
     public void NotifyMissile()
     {
-        Notify("GenerateMissile;" + Direction + ";" + 3);
+        Notify("GenerateMissile;" + Direction.x + ";" + Direction.y + ";"+ Direction.z + ";" + 3);
     }
 
 }
