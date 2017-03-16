463683ada7ae7705c1f57e951daf2744a786eba3
GoodBye-CurelWorld <421790777@qq.com>
2017-03-16 16:06:21 +0800

    yyf-3.16.2

---
+++ Scripts/Utils/GroundObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundObject : MonoBehaviour {
+
+
+	void FixedUpdate () {
+        Vector3 temp = this.transform.position;
+        temp.z = temp.y;
+        this.transform.position = temp;
+
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+	}
+}
