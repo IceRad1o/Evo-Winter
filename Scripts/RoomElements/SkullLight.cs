f8503f105b3cff3b5cd22c671ed5be8652ffb35d
1i9ht <846017531@qq.com>
2017-03-22 20:27:06 +0800

    LKS3.22

--- Scripts/RoomElements/SkullLight.cs
+++ Scripts/RoomElements/SkullLight.cs
@@ -3,15 +3,46 @@ using System.Collections;
 
 public class SkullLight : RoomElement {
 
-	// Use this for initialization
+    public Animator animator;
+    private int isBroken;
     public override void Awake()
     {
         base.Awake();
         RoomElementID = 9;
 	}
-	
-	// Update is called once per frame
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isBroken = 0;
+    }
+
+
 	void Update () {
 	
 	}
+
+        //碰撞检测
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            if (other.GetComponentInParent<Character>().IsWeaponDmg > 0 && isBroken == 0 && other.GetComponentInParent<Character>().Camp == 0)
+            {
+                // if (Player.Instance.Character.FaceDirection < 0) ;
+                animator.SetTrigger("destory");
+                //else
+                //animator.SetTrigger("destory2");
+                isBroken = 1;
+            }
+            else
+            {
+                
+            }
+        }
+
+    }
+
+
+
 }
