852b149db45dd84907b7ca4e4a369986e7628de1
GoodBye-CurelWorld <421790777@qq.com>
2017-03-25 16:59:13 +0800

    yyf-3.25

--- Scripts/Characters/HurtByContract.cs
+++ Scripts/Characters/HurtByContract.cs
@@ -1,47 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/// <summary>
+/// 碰撞产生伤害脚本.
+/// </summary>
 public class HurtByContract : MonoBehaviour
 {
 
-    public int camp;
+    public string[] destTags;
+    /// <summary>
+    /// 伤害:0-999
+    /// </summary>
+    public int damage = 1;
+    /// <summary>
+    /// 击退效果等级:0-3
+    /// </summary>
+    public int beatBackLevel = 0;
+    /// <summary>
+    /// 击倒效果等级:0-3
+    /// </summary>
+    public int beatDownLevel = 0;
     /// <summary>
     /// 造成伤害后是否销毁
     /// </summary>
-    public int isDestory=0;
-    public GameObject HitPrefab;
-    private Vector3 tempPosition;
-    private Vector3 lastPosition;
-    // Use this for initialization
-    void Start()
-    {
+    public int isDestory = 0;
+    /// <summary>
+    /// 造成伤害时产生的特效
+    /// </summary>
+    public GameObject hitPrefab;
 
-    }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        //记录下之前的位置
-        lastPosition = tempPosition;
-        tempPosition = transform.position;
-    }
 
     /// <summary>
-    /// 2D碰撞检测
+    /// 3D碰撞检测,对于不同物体有
     /// </summary>
     /// <param name="other">与其碰撞的GameObj</param>
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter(Collider other)
     {
+        string destTag=other.tag;
 
-        if ((other.tag == "EnemyRB2D" && camp == 0) || (other.tag == "PlayerRB2D" && camp == 1))
+        for (int i = 0; i < destTags.Length;i++ )
         {
-            //Debug.Log("Enemy hurt!" + other.GetComponent<Character>().Health);
-            other.GetComponentInParent<Character>().Health--;
-            
+            if(destTag==destTags[i])
+            {
+                Character ch = other.GetComponent<Character>();
+                if (ch.IsAlive < 0 || ch.Invincible == 1)
+                    return;
+                ch.Health -= damage;
+                if (beatBackLevel > 0)
+                {
+
+                }
+                if (beatDownLevel > 0)
+                {
+
+                }
+                if(hitPrefab!=null)
+                {
+                    Instantiate(hitPrefab, this.transform.position, Quaternion.identity);
+                }
+                if (isDestory != 0)
+                    Destroy(gameObject);
+            }
         }
 
-        if (isDestory != 0)
-            Destroy(gameObject);
+        return;
+
+
+
+
+ 
     }
 
 
