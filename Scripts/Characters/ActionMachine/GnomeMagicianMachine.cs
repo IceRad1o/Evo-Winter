69c762c4f9c800f0dac08ad7b80fac96efae9174
GoodBye-CurelWorld <421790777@qq.com>
2017-03-26 14:28:17 +0800

    yyf-3.26-2

--- Scripts/Characters/ActionMachine/GnomeMagicianMachine.cs
+++ Scripts/Characters/ActionMachine/GnomeMagicianMachine.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GnomeMagicianMachine : MonoBehaviour {
+public class GnomeMagicianMachine : ActionStateMachine
+{
 
 	// Use this for initialization
 	void Start () {
