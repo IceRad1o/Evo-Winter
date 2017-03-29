ab3c6962ed626f46f95123a3852335c669ed68a6
GoodBye-CurelWorld <421790777@qq.com>
2017-03-29 21:15:15 +0800

    yyf-3.29-4

--- Scripts/Camera/CameraController.cs
+++ Scripts/Camera/CameraController.cs
@@ -6,8 +6,10 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
 
     private Vector3 offset;
+    private Vector3 delta;
 	// Use this for initialization
 	void Start () {
+        delta = new Vector3(5, 0, 0);
         offset = transform.position - player.transform.position;
 	}
 	
@@ -18,6 +20,13 @@ public class CameraController : MonoBehaviour {
 
     void LateUpdate()
     {
+
         transform.position = player.transform.position + offset;
+        if (transform.position.x > 6.8f)
+            transform.position= new Vector3(6.8f, transform.position.y,transform.position.z);
+        if (transform.position.x < -6.8f)
+            transform.position = new Vector3(-6.8f, transform.position.y, transform.position.z);
+        if(transform.position.y < -1.5f)
+            transform.position = new Vector3(transform.position.x, -1.5f, transform.position.z);
     }
 }
