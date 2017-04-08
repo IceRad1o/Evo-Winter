23ab4d1a08d23a810241be4172e41b1fc33d1244
cen20081007 <cen20081007@163.com>
2017-04-08 16:27:26 +0800

    Cen-4.3

--- Scripts/Buff/BuffTiming/HardArmor.cs
+++ Scripts/Buff/BuffTiming/HardArmor.cs
@@ -6,6 +6,6 @@ public class HardArmor : BuffTiming {
 
     public override void Create(int ID, string spTag = "")
     {
-        //this.GetComponent<Character>()  
+        //this.GetComponent<Character>().Be
     }
 }
