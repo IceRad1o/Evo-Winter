c92ad7e07da03ee6cfe8295e646397b3e47e8cfa
1i9ht <846017531@qq.com>
2017-04-23 00:19:27 +0800

    LKS2017.4.23.0

--- Scripts/Characters/RougeLycanJ.cs
+++ Scripts/Characters/RougeLycanJ.cs
@@ -39,4 +39,47 @@ public class RougeLycanJ : MonoBehaviour {
 			GameObject particle = Instantiate (RougeLaycanParticle [0], playerPos, Quaternion.identity) as GameObject;
 		}
 	}
+
+	//生成火焰
+	void LaycanFire(){
+		if (EnemyManager.Instance.EnemyList.Count > 0) {
+			int near = 0;
+			//找到最近的怪
+			for (int i = 1; i < EnemyManager.Instance.EnemyList.Count; i++) {
+				if (Mathf.Abs(EnemyManager.Instance.EnemyList [i].transform.position.x - this.transform.position.x)
+					< Mathf.Abs(EnemyManager.Instance.EnemyList [near].transform.position.x- this.transform.position.x))
+					near = i;
+			}
+
+			Vector3 enemyPos = EnemyManager.Instance.EnemyList [near].transform.position;
+			GameObject particle = Instantiate (RougeLaycanParticle [1], enemyPos, Quaternion.identity) as GameObject;
+		}
+	}
+
+	//瞬移
+	Vector3 flashPos = new Vector3();
+	float flashDis = 2f;
+	//向前移动
+	void FlashMoveGo(){
+		flashPos = Player.Instance.Character.transform.position;
+		//Player.Instance.Character.transform.position = new Vector3 (
+			//Player.Instance.Character.transform.position.x+Player.Instance.Character.FaceDirection*flashDis,
+			//Player.Instance.Character.transform.position.y,
+			//Player.Instance.Character.transform.position.z);
+
+		//朝向相反
+		Player.Instance.Character.CanMove = 1;
+		Player.Instance.Character.Direction = new Vector3 (Player.Instance.Character.FaceDirection * -1f, 0, 0);
+		Player.Instance.Character.CanMove = 0;
+
+	}
+
+	//移动回来
+	void FlashMoveBack(){
+		Player.Instance.Character.transform.position = flashPos;
+		//朝向相反
+		Player.Instance.Character.CanMove = 1;
+		Player.Instance.Character.Direction = new Vector3 (Player.Instance.Character.FaceDirection * -1f, 0, 0);
+		Player.Instance.Character.CanMove = 0;
+	}
 }
