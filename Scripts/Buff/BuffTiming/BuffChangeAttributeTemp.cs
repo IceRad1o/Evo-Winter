96ea8353e414cb064be236014bd3916c9bf6da1b
cen20081007 <cen20081007@163.com>
2017-04-23 20:05:46 +0800

    Cen-4.23

--- Scripts/Buff/BuffTiming/BuffChangeAttributeTemp.cs
+++ Scripts/Buff/BuffTiming/BuffChangeAttributeTemp.cs
@@ -63,8 +63,10 @@ public class BuffChangeAttributeTemp : BuffTiming {
                 break;
             case 2:
                 this.gameObject.GetComponent<Character>().MoveSpeed += dValue;
+
                 if (dValue >= 0)
                 {
+                    Debug.Log("Speed UP");
                     GameObject pfb = Resources.Load("Buffs/SpeedDown") as GameObject;
                     Vector3 s = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, -1);
                     prefabInstance = Instantiate(pfb);
