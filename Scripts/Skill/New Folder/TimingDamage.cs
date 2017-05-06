a5a432d29d01b64520483d13b32f01cc49032673
cen20081007 <cen20081007@163.com>
2017-05-07 03:33:15 +0800

    Cen-5.7

---
+++ Scripts/Skill/New Folder/TimingDamage.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimingDamage : MonoBehaviour {
+
+
+    public string[] damageTag;
+
+    public float interval=3;
+
+    public List<GameObject> listEnemy = new List<GameObject>();
+
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        foreach (var item in damageTag)
+        {
+            if (other.tag == item)
+            {
+                foreach (GameObject t in listEnemy)
+                {
+                    if (t == other.gameObject)
+                        return;
+                }
+                listEnemy.Add(other.gameObject);
+                other.gameObject.GetComponent<Character>().Health--;
+
+                StartCoroutine(delay());
+            }
+        }
+
+        
+
+    }
+
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(interval);
+        listEnemy.RemoveAt(0);
+    }
+}
