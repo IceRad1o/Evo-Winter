89310bb8431272f7e6975bdfd6683730e76c5f01
1i9ht <846017531@qq.com>
2017-05-14 15:58:39 +0800

    LKS5.14.5

--- Scripts/RoomElements/SkullLight.cs
+++ Scripts/RoomElements/SkullLight.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class SkullLight : RoomElement {
 
     public Animator animator;
-    private int isBroken;
     public AudioClip putOut;
     public override void Awake()
     {
@@ -16,7 +15,6 @@ public class SkullLight : RoomElement {
     void Start()
     {
         animator = GetComponent<Animator>();
-        isBroken = 0;
     }
 
 
@@ -24,33 +22,72 @@ public class SkullLight : RoomElement {
 	
 	}
 
-        //碰撞检测
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Weapon")
-        {
-            if (other.GetComponentInParent<Character>().IsWeaponDmg > 0 && isBroken == 0 && other.GetComponentInParent<Character>().Camp == 0)
-            {
-              
-                if (Player.Instance.Character.FaceDirection < 0) animator.SetTrigger("destory");
-                else animator.SetTrigger("destory2");
-                isBroken = 1;
-            }
-            else
-            {
-                
-            }
-        }
-		if (other.tag == "Missile") 
+    //碰撞检测
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.tag == "Weapon")
+//        {
+//            if (other.GetComponentInParent<Character>().IsWeaponDmg > 0 && other.GetComponentInParent<Character>().Camp == 0)
+//            {
+//              
+//                if (Player.Instance.Character.FaceDirection < 0) animator.SetTrigger("destory");
+//                else animator.SetTrigger("destory2");
+//            }
+//            else
+//            {
+//                
+//            }
+//        }
+//		if (other.tag == "Missile") 
+//		{
+//			if(other.transform.position.x - this.transform.position.x>0)
+//				animator.SetTrigger("destory");
+//			else
+//				animator.SetTrigger("destory2");
+//		}
+//
+//    }
+
+	//碰撞检测
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (RoomElementState == 1)
+			return;
+		if (collision.gameObject.CompareTag("Player"))
 		{
-			if(other.transform.position.x - this.transform.position.x>0)
+			Player.Instance.Character.AddObserver(this);
+			RoomManager.Instance.Notify("HitSkull");
+		}
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		RoomManager.Instance.Notify("LeaveSkull");
+		if (collision.gameObject.tag == "Player")
+		{
+			Player.Instance.Character.RemoveObserver(this);
+		}
+	}
+
+	//动画
+	private void HitScull(Collision collision)
+	{
+		if (RoomElementState == 1)
+			return;
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			if (Player.Instance.Character.FaceDirection < 0) animator.SetTrigger("destory");
+			else animator.SetTrigger("destory2");
+		}
+		if (collision.gameObject.CompareTag("Missile")) 
+		{
+			if(collision.gameObject.transform.position.x - this.transform.position.x>0)
 				animator.SetTrigger("destory");
 			else
 				animator.SetTrigger("destory2");
 		}
-
-    }
-
+	}
+	//声音
     void Sound()
     {
         SoundManager.Instance.PlaySoundEffect(putOut);
