a987aa433c4103421bfc9e7b52df447f9374600e
1i9ht <846017531@qq.com>
2017-05-21 12:55:38 +0800

    LKS5.14.4

---
+++ Scripts/Characters/Skill/ArcherLycanJ.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArcherLycanJ : MonoBehaviour {
+
+	public float fearRangeX = 2.5f;
+	public float fearRangeY = 1f;
+	public GameObject ArcherLycanParticle;
+
+
+	void ArcherLycanJJ()
+	{
+		//生成粒子
+		Vector3 playerPos = Player.Instance.Character.transform.position;
+		GameObject particle = Instantiate (ArcherLycanParticle, playerPos, Quaternion.identity) as GameObject;
+
+		//判断恐惧
+		FindFear();
+	}
+
+	//判断恐惧
+	void FindFear()
+	{
+		if (EnemyManager.Instance.EnemyList.Count > 0) {
+
+			//找到恐惧范围内的怪
+			for (int i = 1; i < EnemyManager.Instance.EnemyList.Count; i++) 
+			{
+				if (Mathf.Abs (EnemyManager.Instance.EnemyList [i].transform.position.x - this.transform.position.x) < fearRangeX &&
+					Mathf.Abs (EnemyManager.Instance.EnemyList [i].transform.position.y - this.transform.position.y) < fearRangeY)
+				{
+					Fear (i);
+				}
+			}
+
+
+		}
+	}
+
+	//恐惧
+	void Fear(int i)
+	{
+		EnemyManager.Instance.EnemyList [i].CanMove = 0;
+		Debug.Log ("Fear:"+i);
+		if (Player.Instance.transform.position.x > EnemyManager.Instance.EnemyList [i].transform.position.x)
+		{
+			if(EnemyManager.Instance.EnemyList [i].FaceDirection>0)
+				EnemyManager.Instance.EnemyList [i].FlashBy (-15030);
+			else
+				EnemyManager.Instance.EnemyList [i].FlashBy (15030);
+		} 
+		else 
+		{
+			if(EnemyManager.Instance.EnemyList [i].FaceDirection>0)
+				EnemyManager.Instance.EnemyList [i].FlashBy (15030);
+			else
+				EnemyManager.Instance.EnemyList [i].FlashBy (-15030);
+		}
+
+		EnemyManager.Instance.EnemyList [i].CanMove = 1;
+	}
+}
